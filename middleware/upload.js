027361cd76ba3aeaa430ad3a04a9bcd4a48e859a
Rouhan Insights <rouhaniinsights2025@gmail.com>
2025-08-15 04:14:35 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,6 +1,7 @@
 // middleware/upload.js
 const multer = require("multer");
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({ storage ,
+                      limits: { fileSize: 5 * 1024 * 1024 },});
 
 module.exports = upload;
