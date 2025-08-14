9a0c8e0205152c9dc41f110c8120b591a01791fe
Rouhan Insights <rouhaniinsights2025@gmail.com>
2025-08-15 04:51:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const pool = require('./config/db');
+
+
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 
 const app = express();
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
 app.use(express.json());
 app.use('/api/orders', require('./routes/orderRoutes'));
