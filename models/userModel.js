9fb724008e151b4bcf26c0bac84ffd23bd7aeee9
Rouhan Insights <rouhaniinsights2025@gmail.com>
2025-08-15 16:41:23 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const createUser = async ({ name, email, phone, passwordHash, vehicle, govtId, u
   const query = `
     INSERT INTO da_users (name, email, phone, password_hash, vehicle_details, govt_id, availability, upload_img)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-    RETURNING user_id, name, email, phone, vehicle_details, upload_img, created_at
+    RETURNING user_id, name, email, phone,password_hash, vehicle_details, upload_img, created_at
   `;
   const values = [name, email, phone, passwordHash, vehicle, govtId, true, upload_img];
   const result = await pool.query(query, values);
